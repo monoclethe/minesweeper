d36b4d7d329bffe2c0ce5d5a8fb14d7463abc91b
monoclethe <jwrocksyt@gmail.com>
2024-03-02 21:39:18 -0500

    basically the whole game

--- index.js
+++ index.js
@@ -4,45 +4,98 @@ var dimX = document.getElementById("dimX");
 var dimY = document.getElementById("dimY");
 var board = document.getElementById("board");
 var mines = document.getElementById("mines");
+var minedisp = document.getElementById("minedisp");
+var timedisp = document.getElementById("timedisp")
 
+var timerAgent;
+var t = 0;
+var md = 0;
+
+const mod = [0, -1, 1, dimX.value, dimX.value - 1, dimX.value + 1, -dimX.value, -dimX.value - 1, -dimX.value + 1]
 var boardArray = []
 
 function start () {
+    try{
+        clearInterval(timerAgent);
+    } catch {}
+    t = 0;
+    var timerAgent = setInterval(function () {
+        t++;
+        timedisp.innerHTML = t;
+    }, 1000)
+    md = 0;
+    let cx = dimX.value * dimY.value
+    mines.value = cx/5
+    document.getElementById("board").style="background-color:##447044;";
     boardArray = [];
-    let boardTable = "<table>"
+    let boardTable = "<table id=\"gameTable\">"
     let ratio = mines.value / (dimX.value * dimY.value);
     for (let x=0; x < dimX.value; x++) {
         boardTable += "<tr>"
         for (let y=0; y < dimY.value; y++) {
             if (Math.random() < ratio) {
                 boardArray.push(9);
+                md++;
             } else {
                 boardArray.push(0);
             }
-            boardTable += "<td class=\"unchecked\" id=" + x + "," + y + " onclick=recieve(" + x + "," + y + ")></td>"
+            boardTable += "<td class=\"unchecked\" id=" + x + "," + y + " onclick=recieve(" + x + "," + y + ") oncontextmenu=\"mark(" + x + "," + y + ");return false;\"></td>"
         }
         boardTable += "</tr>"
     }
     boardTable += "</table>"
     board.innerHTML = boardTable;
+
+    let n;
+
+    minedisp.innerHTML = md;
+
+    n = Math.floor(Math.random() * dimX.value * dimY.value);
+    boardArray[n] = 0;
+    boardArray[n-1] = 0;
+    boardArray[n+1] = 0;
+    boardArray[n+dimX.value] = 0;
+    boardArray[n-1+dimX.value] = 0;
+    boardArray[n+1+dimX.value] = 0;
+    boardArray[n-dimX.value] = 0;
+    boardArray[n-1-dimX.value] = 0;
+    boardArray[n+1-dimX.value] = 0;
+
     for (let x = 0; x < dimX.value; x++) {
         for (let y=0; y<dimY.value; y++) {
             boardArray[y * dimX.value + x] = countMines(x, y);
         }
     }
+
+
+    clear(convertnumtoxy(n)[0], convertnumtoxy(n)[1]);
+    clearAround(convertnumtoxy(n)[0], convertnumtoxy(n)[1]);
+
+    
 }
 
 function countMines (x, y) {
-    let pos = y * dimX.value + x
-    
-    if (boardArray[pos] !== 9) {
+    if (boardArray[y * dimX.value + x] !== 9) {
+        let pos = y * dimX.value + x;
+        let b = [-1, 1, -1, 1];
+
+        if (y === 0) {
+            b[0] = 0;
+        }
+        if (y === dimY.value - 1) {
+            b[1] = 0;
+        }
+        if (x === 0) {
+            b[2] = 0;
+        }
+        if (x === dimX.value - 1) {
+            b[3] = 0
+        }
         let total = 0;
-        for (let i = -1; i < 2; i++) {
-            for (let j = -1; j < 2; j++) {
-                if (i + j * dimX.value + pos >= 0) {
-                    if (boardArray[i + j * dimX.value + pos] === 9) {
-                        total++;
-                    }
+        for (let i = b[0]; i <= b[1]; i++) {
+            for (let j = b[2]; j <= b[3]; j++) {
+                if (boardArray[pos + dimX.value * i + j] === 9) {
+                    total++;
                 }
             }
         }
@@ -52,9 +105,74 @@ function countMines (x, y) {
     }
 }
 
-function recieve(x, y) {
-    console.log(boardArray[y * dimX.value + x]);
-    if (boardArray[y * dimX.value + x] === 9) {
-        document.getElementById(x + "," + y).style="background-color: red;";
+function clearAround(x, y) {
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            if (boardArray[convertxytonum(x + i, y + j)] === 0) {
+                const element = document.getElementById((x + i) + "," + (y + j));
+                if (element && element.classList.contains("unchecked")) {
+                    clear(x + i, y + j);
+                    clearAround(x + i, y + j);
+                } 
+            } else if (boardArray[convertxytonum(x + i, y + j)] !== 9){
+                clear(x + i, y + j);
+            }
+        }
+    }
+}
+
+function clear(x, y) {
+    const element = document.getElementById(x + "," + y);
+    if (element) {
+        element.classList.add("checked");
+        element.classList.remove("unchecked");
+        element.innerHTML = "<img src=\"images/tile" + boardArray[y * dimX.value + x] + ".png\">";
+    }
+}
+
+function convertxytonum (x, y) {
+    return y * dimX.value + x;
+    
+}
+
+function convertnumtoxy (num) {
+    return [num % dimX.value, Math.floor(num / dimX.value)]
+}
+
+function recieve (x, y) {
+    if (!(document.getElementById(x + "," + y).classList.contains("flag"))) {
+        document.getElementById(x + "," + y).classList.add("checked");
+        document.getElementById(x + "," + y).classList.remove("unchecked");
+        document.getElementById(x + "," + y).innerHTML = "<img src=\"images/tile" + boardArray[y * dimX.value + x] + ".png\">";
+        console.log(boardArray[y * dimX.value + x]);
+        if (boardArray[y * dimX.value + x] === 9) {
+            gameEnd();
+        } else if (boardArray[y * dimX.value + x] === 0) {
+            clearAround(x, y);
+        }
+    }
+}
+
+function mark (x, y) {
+    let tile = document.getElementById(x + "," + y);
+    if (tile.classList.contains("unchecked") && !(tile.classList.contains("flag"))) {
+        tile.classList.add("flag");
+        md -= 1;
+        minedisp.innerHTML = md;
+        tile.innerHTML = "<img src=\"images/tile10.png\">";
+    } else if (tile.classList.contains("unchecked") && tile.classList.contains("flag")) {
+        tile.classList.remove("flag");
+        tile.innerHTML = "";
+        md += 1;
+        minedisp.innerHTML = md;
+    }
+}
+function gameEnd () {
+    clearInterval(timerAgent);
+    for (let i = 0; i < dimX.value; i++) {
+        for (let j = 0; j < dimY.value; j++) {
+            clear(i, j);
+            document.getElementById("board").style="background-color:#FF0000;";
+        }
     }
 }
